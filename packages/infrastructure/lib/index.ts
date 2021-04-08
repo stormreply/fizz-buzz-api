49d648dc0bc9fef8251b24327b4a466e68dbf6a2
femonds <f.emonds@reply.de>
2021-04-08 15:53:51 +0200

    feat(fizz-buzz-api): added api gw and number script

--- packages/infrastructure/lib/index.ts
+++ packages/infrastructure/lib/index.ts
@@ -1,15 +1,26 @@
 import { Code, Function, Runtime } from '@aws-cdk/aws-lambda';
 import { Construct, Stack, StackProps } from '@aws-cdk/core';
+import { LambdaRestApi } from '@aws-cdk/aws-apigateway';
+import { RetentionDays } from '@aws-cdk/aws-logs';
 
 import path from 'path';
 export class FizzBuzzApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new Function(this, 'FizzBuzzAPILambda', {
+    const fizzbuzz = new Function(this, 'FizzBuzzAPILambda', {
       runtime: Runtime.NODEJS_14_X,
       code: Code.fromAsset(path.dirname(require.resolve('@stormreply/fizz-buzz-api-lambda/build/package.json'))),
       handler: 'src/index.default',
+      logRetention: RetentionDays.ONE_WEEK,
     });
+
+    const fizzbuzzApi = new LambdaRestApi(this, 'fizzbuzz-apigw', {
+      handler: fizzbuzz,
+      proxy: false,
+    });
+
+    const check = fizzbuzzApi.root.addResource('check');
+    check.addMethod('POST');
   }
 }
--- packages/lambda/test/index.test.ts
+++ packages/lambda/test/index.test.ts
@@ -4,11 +4,43 @@ import lambdaTester from 'lambda-tester';
 import fizzBuzzApiHandler from '../src';
 
 describe('Fizz Buzz API handler', function () {
-  test('verifies successful response', async () => {
+  test('verifies not matching response', async () => {
     return lambdaTester(fizzBuzzApiHandler)
-      .event({} as APIGatewayProxyEventV2)
+      .event(({ body: JSON.stringify({ number: 22 }) } as unknown) as APIGatewayProxyEventV2)
       .expectResult((result: APIGatewayProxyResultV2<string>) => {
-        expect(result).toStrictEqual('Hello World!');
+        expect(result).toStrictEqual('Not matching');
+      });
+  });
+
+  test('verifies fizzbuzz response', async () => {
+    return lambdaTester(fizzBuzzApiHandler)
+      .event(({ body: JSON.stringify({ number: 15 }) } as unknown) as APIGatewayProxyEventV2)
+      .expectResult((result: APIGatewayProxyResultV2<string>) => {
+        expect(result).toStrictEqual('fizzbuzz');
+      });
+  });
+
+  test('verifies fizz response', async () => {
+    return lambdaTester(fizzBuzzApiHandler)
+      .event(({ body: JSON.stringify({ number: 21 }) } as unknown) as APIGatewayProxyEventV2)
+      .expectResult((result: APIGatewayProxyResultV2<string>) => {
+        expect(result).toStrictEqual('fizz');
+      });
+  });
+
+  test('verifies buzz response', async () => {
+    return lambdaTester(fizzBuzzApiHandler)
+      .event(({ body: JSON.stringify({ number: 25 }) } as unknown) as APIGatewayProxyEventV2)
+      .expectResult((result: APIGatewayProxyResultV2<string>) => {
+        expect(result).toStrictEqual('buzz');
+      });
+  });
+
+  test('verifies empty body response', async () => {
+    return lambdaTester(fizzBuzzApiHandler)
+      .event(({} as unknown) as APIGatewayProxyEventV2)
+      .expectResult((result: APIGatewayProxyResultV2<string>) => {
+        expect(result).toStrictEqual('No number!');
       });
   });
 });
